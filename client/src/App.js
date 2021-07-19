ec722b16129e430136889583f59dd013def8ed81
Keith Ricker <Keith.Ricker@abelsontaylor.com>
2021-07-19 02:58:11 -0500

    adding lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-import ContactPage from './pages/contact/contact.component';
 import { selectCurrentUser } from './redux/user/user.selector';
 import { checkUserSession } from './redux/user/user.actions'
+
+const HomePage = lazyLoad(() => import('./pages/homepage/homepage.component'))
+const SignInAndSignUpPage = lazyLoad(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazyLoad(() => import('./pages/checkout/checkout.component'));
+const ContactPage = lazyLoad(() => import('./pages/contact/contact.component'));
+const ShopPage = lazyLoad(() => import('./pages/shop/shop.component'));
+const Header = lazyLoad(() => import('./components/header/header.component'));
+
+function lazyLoad(importFunc) {
+  const Component = React.lazy(importFunc)
+  return () => <Suspense fallback={<div>Loading ...</div>}><Component /></Suspense>
+}
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -31,7 +37,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/contact' component={ContactPage} />
+          <Route exact path='/contact' component={() => <ContactPage />} />
           <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={
             () => this.props.currentUser ? (

c7926702cfd63040a1dfc82e387cf369ae740487
keithricker <keithricker@gmail.com>
2023-06-08 16:50:09 -0500

    tweaking the dev server start script.

--- scripts/dev-start.sh
+++ scripts/dev-start.sh
@@ -1,5 +1,5 @@
 if [ ! -d "./client/node_modules" ]; then 
-  cd ./client && npm install && npm start && cd ../
+  cd ./client && npm run-script install && npm start && cd ../ & npm start
 else 
-  cd ./client && npm start && cd ../
+  cd ./client && npm start && cd ../ & npm start
 fi
